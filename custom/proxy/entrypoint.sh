f0ea990c460515f743a6e806ed9a37eaf30f7f8f
root <root@ip-10-10-1-101.us-west-2.compute.internal>
2016-05-16 15:38:40 +0000

    migrated to github

---
+++ custom/proxy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+ 
+cp -R /resources/configuration/* /etc/nginx/
+cp -R /resources/release_note/* /usr/share/nginx/html/
+ 
+# Copy and replace tokens
+perl -p -i -e 's/###([^#]+)###/defined $ENV{$1} ? $ENV{$1} : $&/eg' < "/templates/configuration/nginx.conf" 2> /dev/null 1> "/etc/nginx/nginx.conf"
+
+/usr/sbin/nginx
