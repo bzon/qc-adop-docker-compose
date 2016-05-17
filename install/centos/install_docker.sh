d0cb6afadd36f75013f641e2e2d2d01a5d228aaa
root <root@ip-10-10-1-101.us-west-2.compute.internal>
2016-05-17 03:21:50 +0000

    added installation of docker compose

--- install/centos/install_docker.sh
+++ install/centos/install_docker.sh
@@ -30,3 +30,6 @@ LimitCORE=infinity
 TimeoutStartSec=0
 EOF
 
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose && ln -sv /usr/local/bin/docker-compose /usr/bin/docker-compose
