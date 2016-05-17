d480190d8dc0568d418388ea971e1ee64646110c
root <root@ip-10-10-1-101.us-west-2.compute.internal>
2016-05-17 12:59:50 +0000

    added job-dsl plugins fix

---
+++ compose.env.sh
@@ -0,0 +1,14 @@
+
+export PUBLIC_IP=52.36.240.34
+export TARGET_HOST=52.36.240.34
+export LOGSTASH_HOST=52.36.240.34
+export VOLUME_DRIVER=local
+export CUSTOM_NETWORK_NAME=adop-network-overlay
+export AWS_REGION=us-west-2
+export VPC_ID=vpc-62912306
+export DEFAULT_PUBLIC_SUBNET_ID=subnet-de38e8a8
+export DEFAULT_PRIVATE_SUBNET_ID=subnet-df38e8a9
+export DEFAULT_RHEL_AMI=ami-775e4f16
+export DEFAULT_SECURITY_GROUP_ID=sg-07253f60
+export DEFAULT_KEY_PAIR=oregon_oracle_key
+
