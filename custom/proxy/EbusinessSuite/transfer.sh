f6eb07ae64741ce4549208f8354fe20115accc01
john.bryan.j.sazon <john.bryan.j.sazon@accenture.com>
2016-05-24 00:42:06 +0800

    Commented out copying nginx conf files for ricewmanager and csvn

--- custom/proxy/EbusinessSuite/transfer.sh
+++ custom/proxy/EbusinessSuite/transfer.sh
@@ -5,7 +5,8 @@ export DOCKER_HOST=""
 docker cp release_note/plugins.json proxy:/resources/release_note/plugins.json
 docker cp release_note/img/csvn.png proxy:/resources/release_note/img/csvn.png
 docker cp release_note/img/ebs.png proxy:/resources/release_note/img/ebs.png
-docker cp configuration/sites-enabled/ricewmanager.conf proxy:/resources/configuration/sites-enabled/ricewmanager.conf
-docker cp configuration/sites-enabled/csvn.conf proxy:/resources/configuration/sites-enabledcsvn.conf
+## Currently disabled unless ricewmanager docker image is permitted to be public
+#docker cp configuration/sites-enabled/ricewmanager.conf proxy:/resources/configuration/sites-enabled/ricewmanager.conf
+#docker cp configuration/sites-enabled/csvn.conf proxy:/resources/configuration/sites-enabled/csvn.conf
 
 docker restart proxy
